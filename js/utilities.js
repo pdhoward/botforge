5b93a70a2d93e0ceb40a10137e99909d90cf1cb2
pdhoward <patrick.howard@hotmail.com>
2018-07-30 16:23:47 -0400

    updates

--- js/utilities.js
+++ js/utilities.js
@@ -1,16 +1,3 @@
-// Copyright 2017 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 function shuffle(arr) {
 
@@ -91,7 +78,7 @@ var Panel = Class.extend({
     div.draggable({
       start: function() {
         if (app.selectPanel)
-          app.selectPanel(id);        
+          app.selectPanel(id);
       },
       stop: function(ev, ui) {
         panel.savePosition(ui.position.left, ui.position.top, div.width(), div.height());
